88516db89c9c6d38bc36cb8d2bc1e67abd9414b1
faithiheabata <faithiheabata1@gmail.com>
2026-01-12 15:09:50 +0100

    created components in react

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+import WelcomeMessage from './components/WelcomeMessage() 
+function App(){
+    return (
+        <div>
+      <welcomeMessage/>
+
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
