99e224303efbbcb5fbbe442c91d14be14842eee5
faithiheabata <faithiheabata1@gmail.com>
2026-01-13 16:26:55 +0100

    modifications

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import WelcomeMessage from './WelcomeMessage';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-
+import UserProfile  from './UserProfile';
 function App() {
 
   return (
@@ -12,6 +12,7 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
+        <UserProfile />
     </>
   )
 }
