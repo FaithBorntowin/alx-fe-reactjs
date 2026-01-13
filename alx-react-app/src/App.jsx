381b7a36c8d3e3567c3f672dccc099179dc2083c
faithiheabata <faithiheabata1@gmail.com>
2026-01-13 16:39:58 +0100

    modifications

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <MainContent />
         <Footer />
-        <UserProfile />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
