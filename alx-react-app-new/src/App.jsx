f09cd93f808a02dd4e57bc5a54c23260556cd1ea
faithiheabata <faithiheabata1@gmail.com>
2026-01-15 15:08:33 +0100

    usestate hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile  from './UserProfile';
+import Counter from './Counter'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
         <MainContent />
         <Footer />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+       <Counter/>
     </>
   )
 }
