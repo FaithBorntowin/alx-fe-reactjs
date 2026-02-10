25b822345dd5d16c2717c85b87615baaacd6d696
faithiheabata <faithiheabata1@gmail.com>
2026-02-10 14:57:20 +0100

    setting Tailwind css in react project

---
+++ tailwind-react-integration/vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+
+import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ Add this
+
+export default defineConfig({
+  plugins: [
+        react(),
+        tailwindcss(), //
+
+ ],
+})
+
+
+
