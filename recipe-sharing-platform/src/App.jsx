1334c1975d35f7d0e724de28ac72f5840529d094
faithiheabata <faithiheabata1@gmail.com>
2026-02-14 23:34:21 +0100

    integrated tailwindcss into react

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+ function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind is working ✅
+      </h1>
+    </div>
+  );
+}
+
+export default App
